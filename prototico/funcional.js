4303c84f9011caaf2db19ecc6c33a67095b44118
Luiz <luiz@gmail.com>
2024-11-28 10:36:06 -0300

    prototipo

---
+++ prototico/funcional.js
@@ -0,0 +1,5 @@
+// Mensagem no console
+console.log("Bem-vindo ao Vet Vibe!");
+
+// O gradiente dinâmico é gerenciado apenas pelo CSS.
+// Este arquivo está preparado para futuros comportamentos dinâmicos.
